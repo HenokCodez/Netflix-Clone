cf3db4877c2d5e901c90aba853725cf6de835e70
Henok Binayew <henokbinayew21@gmail.com>
2025-02-12 16:27:49 +0300

    my 3rd commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,52 @@
 import React from "react";
+import FooterStyles from "../Footer/Footer.module.css";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
-export default function () {
-  return <div>footer</div>;
+function Footer() {
+  return (
+    <div className={FooterStyles.footerContainer}>
+      <ul>
+        <ul className={FooterStyles.social}>
+          <li>
+            <a href="#">
+              <FacebookIcon />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <InstagramIcon />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <YouTubeIcon />
+            </a>
+          </li>
+        </ul>
+        <li>Audio Description</li>
+        <li>Investor Relations</li>
+        <li>Legal Notice</li>
+        <li>Service Code</li>
+        <div> &copy; 1997 -2025 Netflix, Inc.</div>
+      </ul>
+      <ul>
+        <li>Help Center</li>
+        <li>Jobs</li>
+        <li>Cookie Preference</li>
+      </ul>
+      <ul>
+        <li>Gift Card</li>
+        <li>Term Of Use</li>
+        <li>Corporate Information</li>
+      </ul>
+      <ul>
+        <li> Media Center</li>
+        <li>Privacy</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  );
 }
+export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,59 @@
 import React from "react";
-import "../Header/header.css";
+import { useEffect, useState } from "react";
+import HeaderCss from "../Header/Header.module.css";
+import Logo from "../../assets/logo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
 export default function Header() {
-  return <div>header</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`${HeaderCss.container} ${isScrolled ? HeaderCss.scrolled : HeaderCss.transparent}`}>
+      <div className={HeaderCss.left}>
+        <img src={Logo} alt="Logo" />
+        <ul>
+          <li>Home</li>
+          <li>TVShows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+          <li>MyList</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
+      <div className={HeaderCss.right}>
+        <ul>
+          <li>
+            <SearchIcon className={HeaderCss.icon} />
+          </li>
+          <li>
+            <NotificationsIcon className={HeaderCss.icon} />
+          </li>
+          <li>
+            <AccountBoxIcon className={HeaderCss.icon} />
+          </li>
+          <li>
+            <ArrowDropDownIcon className={HeaderCss.icon} />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
